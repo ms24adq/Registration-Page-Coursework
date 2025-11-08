a2d83d3f5e8883a1601d95069d5e2f7b52aef954
ms24adq <ms24adq@herts.ac.uk>
2025-11-08 16:55:53 +0000

    moved <script></script> from registration.html file to this file

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+        function register() {
+            console.log("Registration triggered by non-semantic element.");
+            document.getElementById('reg-status').textContent = "Registration Successful! See you in October.";
+            document.getElementById('reg-status').classList.remove('hidden');
+        }
+        
+        function clearForm() {
+            document.getElementById('reg-form').reset();
+            document.getElementById('reg-status').textContent = '';
+            document.getElementById('reg-status').classList.add('hidden');
+            console.log("Form cleared.");
+        }
+
